4148b8b0d341cfdf6462dd9938b17fb1ab1e74be
forcathrina <forcathrina@outlook.com>
2024-12-02 18:42:23 +0900

    init

---
+++ qc_grader/__init__.py
@@ -0,0 +1,19 @@
+# (C) Copyright IBM 2020.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+import warnings
+warnings.filterwarnings("ignore")
+
+__version__ = '0.8.8'
+
+challenge_name = 'IBM Quantum Challenge Fall 2021'
+challenge_version = '2021'
+
+from qc_grader.exercises.fall_2021 import *
